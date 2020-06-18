2068e0551778e428d892458e61f46d0fc26bec06
Nguyễn Hoàng Dũng <nhdung1912@gmail.com>
2020-06-18 19:29:17 +0700

    download files

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 import re
+import os
+import sys
 
 def mimetype(content):
     if content.endswith('.css'):
@@ -14,16 +16,44 @@ def mimetype(content):
         return 'text/javascript'
     if content.endswith('.ttf'):
         return 'font/ttf'
+    if content.endswith('.txt'):
+        return 'text/txt'
+    if content.endswith('.pdf'):
+        return 'text/pdf'
+    if content.endswith('.pptx'):
+        return 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+
+
+def render():
+    files = os.listdir('sharing/')
+
+    with open('files.html', 'w') as ifile:
+        ifile.write('<html>')
+        ifile.write('<body>')
+
+        for name in sorted(files):
+            ifile.write(f'<a href = "/sharing/{name}" > {name} </a> <br>')
+
+        ifile.write('<body>')
+        ifile.write('<html>')
+
+    ifile.close()
+
 
 def listening(server):
     while True:
         connection, address = server.accept()
-        print(f'Receive request from {address}')
         request = connection.recv(1024).decode('utf-8')
         request = request.split(' ')
+        print(f'Receive request from {address[0]}:{address[1]} {request[0]} {request[1]}')
         method, content = request[0], request[1].lstrip('/')
 
-        if method == 'GET':
+        if method == 'GET' and content == 'files.html':
+            render()
+            ifile = open(content, 'rb')
+            header = 'HTTP/1.1 200 OK Content-Type: text/html \n\n'
+
+        elif method == 'GET':
             if content == '': content = 'index.html'
             try:
                 ifile = open(content, 'rb')
@@ -32,7 +62,7 @@ def listening(server):
                 ifile = open('404.html', 'rb')
                 header = 'HTTP/1.1 404 Not Found \n\n'
 
-        if method == 'POST':
+        elif method == 'POST':
             data = re.split('=|&', request[-1])
             username, password = data[1], data[3]
             if username == 'admin' and password == 'admin':
